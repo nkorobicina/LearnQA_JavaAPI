024088be05f30cae234385a5633e2162bff2885d
nkorobicina <nkorobicina@github.com>
2022-09-08 13:34:24 +0500

    Задание Ex4: GET-запрос

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -8,5 +10,12 @@ public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
         System.out.println("Hello from Nataly");
+    }
+
+    @Test
+    public void testEx4(){
+        Response response = RestAssured.get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.print();
     }
 }

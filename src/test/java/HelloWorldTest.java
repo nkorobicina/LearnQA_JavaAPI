17546387e900db8ec2196b2b1e81d4e907cf09b1
nkorobicina <nkorobicina@github.com>
2022-09-08 12:59:27 +0500

    Первый коммит

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by user nkorobicina on 08.09.2022.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello, World!");
+    }
+}

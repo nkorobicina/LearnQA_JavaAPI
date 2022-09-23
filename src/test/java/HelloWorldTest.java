eab86e3f4828b7f531bbd3e3963b056eb65c073f
nkorobicina <nkorobicina@github.com>
2022-09-23 16:53:20 +0500

    Занятие 3

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,14 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by user nkorobicina on 08.09.2022.
@@ -25,6 +30,45 @@ public class HelloWorldTest {
         System.out.println(statusCode);
         response.print();
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "John", "Pete"})
+    public void testRestAssured2(String name) {
+        Map<String, String> queryParams = new HashMap<>();
+
+        if(name.length() > 0){
+            queryParams.put("name", name);
+        }
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(queryParams)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        String expectedName = (name.length() > 0) ? name : "someone";
+        assertEquals("Hello, " + expectedName, answer, "The answer is not expected");
+    }
+
+    @Test
+    public void testHelloMethodWithoutName(){
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        assertEquals("Hello, someone", answer, "The answer is not expected");
+    }
+
+    @Test
+    public void testHelloMethodWithName(){
+        String name = "Username";
+        JsonPath response = RestAssured
+                .given()
+                .queryParam("name", name)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        assertEquals("Hello, " + name, answer, "The answer is not expected");
     }
 
     @Test
---
+++ src/test/java/lib/Assertions.java
@@ -0,0 +1,19 @@
+package lib;
+
+import io.restassured.response.Response;
+
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by user nkorobicina on 23.09.2022.
+ */
+public class Assertions {
+
+    public static void assertJsonByName(Response response, String name, int expectedValue) {
+        response.then().assertThat().body("$", hasKey(name));
+
+        int value = response.jsonPath().getInt(name);
+        assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+    }
+}
